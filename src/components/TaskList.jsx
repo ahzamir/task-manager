3d2728e01d1efc8f9a50fa38d37bbf90bb0847b8
Shegofa <shogofadeveloper12@gmail.com>
2024-08-31 13:15:23 +0400

    TaskList added

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,9 +1,31 @@
-const TaskList = () => {
-  return (
-    <div>
-      <h2>Task List</h2>
-    </div>
-  );
-};
+import React from "react";
+   import { useSelector, useDispatch } from "react-redux";
+   import { toggleTask } from "../redux/actions";
 
-export default TaskList;
+   const TaskList = () => {
+     const tasks = useSelector((state) => state.tasks);
+     const dispatch = useDispatch();
+
+     const handleToggleTask = (taskId) => {
+       dispatch(toggleTask(taskId));
+     };
+
+     return (
+       <ul className="task-list">
+         {tasks.map((task) => (
+           <li key={task.id} className="task-item">
+             <input
+               type="checkbox"
+               checked={task.completed}
+               onChange={() => handleToggleTask(task.id)}
+             />
+             <span className={task.completed ? "task-completed" : ""}>
+               {task.title}
+             </span>
+           </li>
+         ))}
+       </ul>
+     );
+   };
+
+   export default TaskList;
