aa60ae8dc069432051ef30a0f91e4066f3f85775
ahzamir <ahmadzamiryousofi1397@gmail.com>
2024-08-31 11:00:57 +0200

    added the main routes and pages for the project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 const App = () => {
   return (
-    <div className="App h-screen text-white bg-black">
-      Hello World
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
